6f2ddced4653d72e0782c5ba4a9631c5f53daca0
Hugo <hugobqdweb@laposte.net>
2022-10-20 00:41:27 +0200

    init tailwind

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,10 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
+import { defineNuxtConfig } from "nuxt";
+
+// https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
+  typescript: {
+    strict: true,
+  },
+  modules: ["@nuxtjs/tailwindcss"],
 
 })
